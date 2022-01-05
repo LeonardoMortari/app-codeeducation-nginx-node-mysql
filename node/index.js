78bf23b2109e0d76e82d191710fdf2e84f0efc2d
root <root@LAPTOP-0VDILRN4.localdomain>
2022-01-04 23:35:22 -0300

    commit

---
+++ node/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const gerador = require('gerador-nome')
+const app = express()
+const port = 3000
+
+const config = {
+    host: 'mysql',
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sqlTabela = `CREATE TABLE IF NOT EXISTS pessoas(
+                        id int primary key auto_increment,
+                        nome varchar(255) not null
+                  )`
+connection.query(sqlTabela)
+
+app.get('/', (req, res) => {
+    const nomeAleatorio = gerador.geradorNome()
+  
+    connection.query(`INSERT INTO pessoas (nome) VALUES ('${nomeAleatorio}')`)
+  
+    connection.query(`SELECT nome FROM pessoas`, (error, results, fields) => {
+      res.send(`
+        <h1>Full Cycle Rocks!</h1>
+        <ul>
+          ${!!results.length ? results.map(el => `<li>${el.nome}</li>`).join('') : ''}
+        </ul>
+      `)
+    })
+})
+
+app.listen(port, () => {
+    console.log('Rodando na porta', + port)
+})
